08e02f1ca031dc9dca2727c3e9fd559e66ae6de8
Petrik Diak <diak@petrik.hu>
2023-11-10 09:51:31 +0100

    debug

--- szabobela_dolgozat/src/Statue.ts
+++ szabobela_dolgozat/src/Statue.ts
@@ -5,9 +5,9 @@ export class Statue implements Artwork {
     height: number;
   
     constructor(public title: string, public year: number, public price: number, height: number) {
-      this.title = title;
-      this.year = year;
-      this.price = price;
-      this.height = height;
+        this.height = height;
+        this.price = price;
+        this.year = year;
+        this.title = title;
     }
   }
--- szabobela_dolgozat/src/main.ts
+++ szabobela_dolgozat/src/main.ts
@@ -3,8 +3,83 @@ import { Artwork } from './Artowrk';
 import { Statue } from './Statue';
 
 const artworks: Artwork[] = [];
+let hiba: string[]=[];
 
 document.addEventListener('DOMContentLoaded',() => {
-    const nev = (document.getElementById('nameID') as HTMLInputElement).value;
-    const ev =  (document.getElementById('yearID') as HTMLInputElement).value;
-})
+    document.getElementById('buttonSubmit')!.addEventListener('click',feltolt);
+});
+
+function feltolt()
+{
+    check();
+    const kiirCount = document.getElementById('countID');
+    const kiirAr = document.getElementById('arID');
+    kiirCount!.textContent = `a művekdarabszáma: `+artworks.length;
+    let kiirAr1 = 0;
+    for (const iterator of artworks) {
+        kiirAr1+=iterator.price;
+    }
+    kiirAr!.textContent = ` A művek összesitett ára: `+kiirAr1 + ` Ft`;
+
+}
+function check()
+{
+    const body = document.getElementById('hibaID')!.innerHTML=``;
+    let checkPass = 0;
+    hiba = [];
+    const nev = (document.getElementById('nameID')! as HTMLInputElement).value;
+    const ev =  parseInt((document.getElementById('yearID')! as HTMLInputElement).value);
+    const ar =  parseInt((document.getElementById('priceID')! as HTMLInputElement).value);
+    const magassag =  parseInt((document.getElementById('heightID')! as HTMLInputElement).value);
+    if(nev.trim().length > 0)
+    {
+        console.log('OK');
+        checkPass++;
+    }
+    else{
+        hiba.push('Hibás név:' + nev); 
+    }
+    if(ev < 2023)
+    {
+        console.log('OK');
+        checkPass++;
+
+    }
+    else{
+        hiba.push('Hibás év: '+ev); 
+    }
+    if (ar >= 5)
+    {
+        console.log('OK');
+        checkPass++;
+
+    }
+    else {
+        hiba.push('Hibás ár: '+ar); 
+    }
+    if (magassag >= 15)
+    {
+        console.log('OK');
+        checkPass++;
+        
+    }
+    else {
+        hiba.push('Hibás magasság: '+magassag);          
+    }
+    if(checkPass==4)
+    {
+        artworks.push(new Statue(nev,ev,ar,magassag));
+        (document.getElementById('nameID') as HTMLInputElement).value ='';
+        (document.getElementById('yearID') as HTMLInputElement).value ='';
+        (document.getElementById('priceID') as HTMLInputElement).value ='';
+        (document.getElementById('heightID') as HTMLInputElement).value ='';
+        const body = document.getElementById('hibaID')!.innerHTML=`Sikeres feltöltés`;
+        checkPass = 0;
+    }
+    else{
+        const body = document.getElementById('hibaID');
+        for (const iterator of hiba) {
+            body!.innerHTML += `<li> ${iterator} </li>`
+        }
+    }
+}
